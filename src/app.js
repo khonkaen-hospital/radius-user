bb78fd150ced55fd80ccbcec605cf0cbee04e285
Sathit Seethaphon <dixonsatit@gmail.com>
2019-11-04 10:56:47 +0700

    add setting page

--- src/app.js
+++ src/app.js
@@ -17,6 +17,14 @@ console.log(env);
 
 const app = remote.app;
 const appDir = jetpack.cwd(app.getAppPath());
+const Store = require('electron-store');
+const schema = {
+	nhso: {
+		cardNo: '',
+		token: ''
+	}
+};
+const store = new Store({schema});
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // files from disk like it's node.js! Welcome to Electron world :)
@@ -27,6 +35,8 @@ const osMap = {
   darwin: "macOS",
   linux: "Linux"
 };
+
+var forms = document.getElementById('settingForm');
 
 function animateCSS(element, animationName, callback) {
   const node = document.querySelector(element)
@@ -48,25 +58,39 @@ let page1 = document.getElementById('page1');
 let page2 = document.getElementById('page2');
 let page3 = document.getElementById('page3');
 
+function goToPage(pageId){
+  let page = document.getElementById(pageId);
+  animateCSS('#page0','slideOutUp',()=>{
+    page0.classList.remove('pageActive');
+    page.classList.add('pageActive');
+    animateCSS('#'+pageId,'slideInUp');
+  });
+}
+
+function back(src){
+  let page = src.srcElement.dataset.page;
+  animateCSS('#'+page,'slideOutDown', () => {
+    document.querySelector('#'+page).classList.remove('pageActive');
+    page0.classList.add('pageActive');
+    animateCSS('#page0','slideInDown');
+  });
+}
+
 document.getElementById('linkPage1').addEventListener('click', (src)=>{
-    animateCSS('#page0','slideOutUp',()=>{
-      page0.classList.remove('pageActive');
-      page1.classList.add('pageActive');
-      animateCSS('#page1','slideInUp');
-    });
+  goToPage('page1');
 });
 document.getElementById('linkPage2').addEventListener('click', (src)=>{
-  animateCSS('#page0','slideOutUp',()=>{
-    page0.classList.remove('pageActive');
-    page2.classList.add('pageActive');
-    animateCSS('#page2','slideInUp');
-  });
+  goToPage('page2');
 });
 document.getElementById('linkPage3').addEventListener('click', (src)=>{
+  goToPage('page3');
+});
+
+document.getElementById('linkPage4').addEventListener('click', (src)=>{
   animateCSS('#page0','slideOutUp',()=>{
     page0.classList.remove('pageActive');
-    page3.classList.add('pageActive');
-    animateCSS('#page3','slideInUp');
+    page4.classList.add('pageActive');
+    animateCSS('#page4','slideInUp');
   });
 });
 
@@ -80,11 +104,39 @@ document.getElementById('back3').addEventListener('click', (src)=>{
   back(src);
 });
 
-function back(src){
-  let page = src.srcElement.dataset.page;
-  animateCSS('#'+page,'slideOutDown', () => {
-    document.querySelector('#'+page).classList.remove('pageActive');
+
+
+function initForm() {
+  let data = store.get('nhso');
+  console.log(data);
+  if(data === undefined){
+    store.set('nhso', {
+      cardNo: '',
+      token: ''
+    });
+  } else {
+    document.getElementById('txtCardNo').value = data.cardNo;
+    document.getElementById('txtToken').value = data.token;
+  }
+}
+
+forms.addEventListener('submit', event => {
+  let cardNo = document.getElementById('txtCardNo').value;
+  let token = document.getElementById('txtToken').value;
+  store.set('nhso', {
+    cardNo: cardNo,
+		token: token
+  });
+  // back to home page
+  animateCSS('#page4','slideOutDown', () => {
+    document.querySelector('#page4').classList.remove('pageActive');
     page0.classList.add('pageActive');
     animateCSS('#page0','slideInDown');
   });
-}
+  event.preventDefault()
+})
+
+initForm();
+
+
+
